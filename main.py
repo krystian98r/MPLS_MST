8ef9de636636f33cfff86de559c053d136a9b417
Krystian Ruczyński <81698619+krystian98r@users.noreply.github.com>
2022-05-12 18:29:37 +0200

    Po 2 zajęciach

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def uzytkownik1():
+    win1 = Tk()
+    win1.title("Użytkownik 1")
+    win1.geometry("300x200")
+    label1 = Label(win1, text="Wiadomość")
+    textarea = Text(win1, bg="white", fg="black", width="30", height="4", padx="5", pady="5")
+    btn = Button(win1, text="Prześlij wiadomość")
+    label2 = Label(win1, text="Wiadomość zwrotna")
+    entry = Entry(win1, bg="white", fg="black", width="24")
 
+    label1.pack()
+    textarea.pack()
+    btn.pack()
+    label2.pack()
+    entry.pack()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def uzytkownik2():
+    win2 = Tk()
+    win2.title("Użytkownik 2")
+    win2.geometry("300x150")
+    label = Label(win2, text="Otrzymana wiadomość")
+    textarea = Text(win2, bg="white", fg="black", width="30", height="4", padx="5", pady="5")
 
+    label.pack()
+    textarea.pack()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def demo():
+    win3 = Tk()
+    win3.title("Przebieg protokołu")
+    win3.geometry("650x300")
+    textarea = Text(win3, bg="white", fg="black", width="80", height="20", padx="20", pady="10")
+    textarea.pack()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    win3.mainloop()
+
+uzytkownik1()
+uzytkownik2()
+demo()
